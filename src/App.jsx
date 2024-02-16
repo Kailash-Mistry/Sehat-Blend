32f57296b850460ab256f923db720470e656355b
Vinayak Gupta <vinayak20.in@gmail.com>
2024-02-16 11:09:30 +0530

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './App.css';
+import Home from "./Pages/Home/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Staffs from "./Pages/Staffs/Staffs";
+import Appointment from "./Pages/Appointments/Appointment";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import AddDoctor from "./Pages/Doctors/AddDoctor/AddDoctor";
+import Registration from "./Pages/Login/Registration/Registration";
+import PatientViewDoctor from "./Pages/Doctors/Doctors/PatientViewDoctor";
+import Login from "./Pages/Login/Login";
+import PageNotFound from "./Pages/PageNotFound/PageNotFound";
+import ViewDoctors from "./Pages/Patients/ViewDoctors/ViewDoctors";
+import DeleteDoctor from "./Pages/Doctors/DeleteDoctor/DeleteDoctor";
+import FindDoctor from "./Pages/Doctors/UpdateDoctor/FindDoctor";
+import AddPatient from "./Pages/Patients/AddPatients/AddPatient";
+import Patients from "./Pages/Patients/Patients/Patients";
+import PatientDetails from "./Pages/Patients/PatientsDetails/PatientDetails";
+import ApproveDoctor from "./Pages/Doctors/ApproveDoctor/ApproveDoctor";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {/* <AuthProvider>
+
+      </AuthProvider> */}
+      <Router>
+        <Routes>
+          {/* NESTED ROUTING APPLIED */}
+          <Route path="/" element={<Dashboard />} >
+            <Route index element={<Home></Home>} />
+            <Route path="doctors" element={<PatientViewDoctor />} />
+            <Route path="addDoctor" element={<AddDoctor />} />
+            <Route path="approveDoctor" element={<ApproveDoctor />} />
+            <Route path="deleteDoctor" element={<DeleteDoctor />} />
+            <Route path="updateDoctor" element={<FindDoctor />} />
+            <Route path="patients" element={<Patients />} />
+            <Route path="addPatient" element={<AddPatient />}>
+              <Route path=":email" element={<AddPatient />} />
+            </Route>
+            <Route path="viewDoctors" element={<ViewDoctors />} />
+            <Route path="patientDetails/:id" element={<PatientDetails />} />
+            <Route path="staffs" element={<Staffs />} />
+            <Route path="appointment" element={<Appointment />}>
+              <Route path=":email" element={<Appointment />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="registration" element={<Registration />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+        <Footer></Footer>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
